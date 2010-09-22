1b9b7534f54fbf27fc6b066360c2ecc1bb6fd2d3
hunter@discursivelabs.com <hunter@discursivelabs.com>
2010-09-22 16:25:55 -0700

    no more echo test

--- createHnWScaleImages.sh
+++ createHnWScaleImages.sh
@@ -10,13 +10,13 @@ COUNT=0
 for i in $(eval echo {$1..$2})
 do
 COUNT=$((COUNT + 1))
-echo ./bin/simple_tree -g /home/programmer/Projects/twitter-txt-rb/twitter-text-rb -H .$i -W .$3 -O  "$COUNT"twitter-txt-rb-h$i-w$3.jpg
+./bin/simple_tree -g /home/programmer/Projects/twitter-txt-rb/twitter-text-rb -H .$i -W .$3 -O  "$COUNT"twitter-txt-rb-h$i-w$3.jpg
 done
 
 # this is the width loop
 for j in $(eval echo {$3..$4})
 do
 COUNT=$((COUNT + 1))
-echo ./bin/simple_tree -g /home/programmer/Projects/twitter-txt-rb/twitter-text-rb -H .$2 -W .$i -O "$COUNT"twitter-txt-rb-h$2-w$j.jpg
+./bin/simple_tree -g /home/programmer/Projects/twitter-txt-rb/twitter-text-rb -H .$2 -W .$i -O "$COUNT"twitter-txt-rb-h$2-w$j.jpg
 done
 
