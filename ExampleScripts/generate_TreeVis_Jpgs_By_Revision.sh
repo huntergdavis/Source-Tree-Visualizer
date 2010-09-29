0318a3c34f309529146bab63176d5f2b8ee97a7e
hunter@discursivelabs.com <hunter@discursivelabs.com>
2010-09-29 16:54:23 -0700

    80mb, not 800 should be used to make gifs

--- ExampleScripts/generate_TreeVis_Jpgs_By_Revision.sh
+++ ExampleScripts/generate_TreeVis_Jpgs_By_Revision.sh
@@ -3,6 +3,6 @@
 cd ~/Projects/source_tree_vis/
 ./bin/simple_tree -r -g ~/Projects/source_tree_vis -O sourceTreeVis.jpg
 cd ~/Projects/source_tree_vis/ExampleScripts
-convert -limit memory 400MiB -limit map 400MiB ~/Projects/source_tree_vis/out/*.jpg sourceTreeVis.gif
+convert -limit memory 40MiB -limit map 40MiB ~/Projects/source_tree_vis/out/*.jpg sourceTreeVis.gif
 
 
