3cd0ceabb7307055d654e3a2af8a167011f420fe
hunter@discursivelabs.com <hunter@discursivelabs.com>
2010-09-29 16:50:27 -0700

    added -limit memory 32MiB -limit map 64MiB options (400mb apiece though for this one)

--- ExampleScripts/generateGifFromJpg.sh
+++ ExampleScripts/generateGifFromJpg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/Projects/source_tree_vis/ExampleScripts
-convert -loop 1 ~/Projects/source_tree_vis/out/*.jpg sourceTreeVis.gif
+convert -limit memory 400MiB -limit map 400MiB -loop 1 ~/Projects/source_tree_vis/out/*.jpg sourceTreeVis.gif
