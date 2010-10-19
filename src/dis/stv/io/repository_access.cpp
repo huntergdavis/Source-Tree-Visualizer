4c0773f53b5f8298f90176c3d5c4a2c9a567e6b8
hunter@discursivelabs.com <hunter@discursivelabs.com>
2010-10-18 18:47:37 -0700

    eliminated redundant code in tree generation this resolved duplicate entry bug as well

--- src/dis/stv/io/repository_access.cpp
+++ src/dis/stv/io/repository_access.cpp
@@ -51,6 +51,9 @@ void RepositoryAccess::InsertByPathName(SurrogateTreeNode* tree, string pathname
 	// should we continue to draw sub leaves?
 	int continueDrawingSubLeaves = drawSubLeafs;
 
+	// our name
+	std::string name;
+
 	// Split off first part of path
 	int firstIndex = pathname.find("/");
 	if(firstIndex == 0)
@@ -62,92 +65,73 @@ void RepositoryAccess::InsertByPathName(SurrogateTreeNode* tree, string pathname
 	}
 	if(firstIndex == -1)
 	{
-		// We have no more path left.  Just a single entry (leaf)
-		// test for keyword failure
-		if(configAgent.DoesThisStringContainFilterKeywords(pathname) < 0)
-		{
-			continueDrawingSubLeaves = 0;
-		}
+		name = pathname;
 
-		if((continueDrawingSubLeaves > 0) || (drawFilteredLeaves == 1))
-		{
-			// We have no more path left.  Just a single entry (leaf)
-			SurrogateTreeNode* file = new SurrogateTreeNode();
-
-			// set all configuration properties from filter items
-			configAgent.AddFilterPropertiesToTreeNode(file,pathname);
-
-			// set other properties on the node
-			string timeStr = boost::lexical_cast<string>(time);
-			file->set(TreeNodeKey::CREATION_TIME, timeStr);
-			file->set(TreeNodeKey::NAME, pathname);
-			file->set(TreeNodeKey::REVISIONCREATED, localRevs);
-			file->set(TreeNodeKey::DRAWNODE, continueDrawingSubLeaves);
-			//printf("Adding node '%s' @ time %ld\n",pathname.c_str(),time);
-			tree->children->push_back(file);
-		}
 	}
 	else
 	{
 		// Split first string out for this nodes name
-		string name = pathname.substr(0,firstIndex);
-		// test for keyword failure  in folder name
-		if(configAgent.DoesThisStringContainFilterKeywords(name) < 0)
-		{
-			continueDrawingSubLeaves = 0;
-		}
-		if((continueDrawingSubLeaves > 0) || (drawFilteredLeaves == 1))
-		{
-			// Look for node in children
-			SurrogateTreeNode* node = NULL;
+		name = pathname.substr(0,firstIndex);
+	}
+	// test for keyword failure  in folder name
+	if(configAgent.DoesThisStringContainFilterKeywords(name) < 0)
+	{
+		continueDrawingSubLeaves = 0;
+	}
+	if((continueDrawingSubLeaves > 0) || (drawFilteredLeaves == 1))
+	{
+		// Look for node in children
+		SurrogateTreeNode* node = NULL;
 
-			vector<SurrogateTreeNode*>::iterator iter = tree->children->begin();
-			for(;iter != tree->children->end(); ++iter)
+		vector<SurrogateTreeNode*>::iterator iter = tree->children->begin();
+		for(;iter != tree->children->end(); ++iter)
+		{
+			SurrogateTreeNode* local = *iter;
+			string nameComp = local->data[TreeNodeKey::NAME];
+			//printf("Comparing %s to %s\n",nameComp.c_str(),name.c_str());
+			if(!nameComp.compare(name))
 			{
-				SurrogateTreeNode* local = *iter;
-				string nameComp = local->data[TreeNodeKey::NAME];
-				//printf("Comparing %s to %s\n",nameComp.c_str(),name.c_str());
-				if(!nameComp.compare(name))
+				// Found node
+				node = (*iter);
+				// Update node time if necessary
+				if(time < atol(node->data[TreeNodeKey::CREATION_TIME].c_str()))
 				{
-					// Found node
-					node = (*iter);
-					// Update node time if necessary
-					if(time < atol(node->data[TreeNodeKey::CREATION_TIME].c_str()))
-					{
-						//printf("Updating time of node[\"%s\"] to %ld from %ld\n", name.c_str(), time, atol(node->data[TreeNodeKey::CREATION_TIME].c_str()));
-						node->set(TreeNodeKey::CREATION_TIME, boost::lexical_cast<string>(time));
-						node->set(TreeNodeKey::REVISIONCREATED, localRevs);
-						node->set(TreeNodeKey::DRAWNODE, continueDrawingSubLeaves);
+					//printf("Updating time of node[\"%s\"] to %ld from %ld\n", name.c_str(), time, atol(node->data[TreeNodeKey::CREATION_TIME].c_str()));
+					node->set(TreeNodeKey::CREATION_TIME, boost::lexical_cast<string>(time));
+					node->set(TreeNodeKey::REVISIONCREATED, localRevs);
+					node->set(TreeNodeKey::DRAWNODE, continueDrawingSubLeaves);
 
-						// set all configuration properties from filter items
-						configAgent.AddFilterPropertiesToTreeNode(node,name);
-					}
-					break;
+					// set all configuration properties from filter items
+					configAgent.AddFilterPropertiesToTreeNode(node,name);
 				}
+				break;
 			}
-			// If child not found, add new node
-			if(node == NULL)
-			{
-				node = new SurrogateTreeNode();
-				string timeStr = boost::lexical_cast<string>(time);
-				node->set(TreeNodeKey::CREATION_TIME, timeStr);
-				node->set(TreeNodeKey::NAME, name);
-				node->set(TreeNodeKey::REVISIONCREATED, localRevs);
-				node->set(TreeNodeKey::DRAWNODE, continueDrawingSubLeaves);
+		}
+		// If child not found, add new node
+		if(node == NULL)
+		{
+			node = new SurrogateTreeNode();
+			string timeStr = boost::lexical_cast<string>(time);
+			node->set(TreeNodeKey::CREATION_TIME, timeStr);
+			node->set(TreeNodeKey::NAME, name);
+			node->set(TreeNodeKey::REVISIONCREATED, localRevs);
+			node->set(TreeNodeKey::DRAWNODE, continueDrawingSubLeaves);
 
-				// set all configuration properties from filter items
-				configAgent.AddFilterPropertiesToTreeNode(node,name);
+			// set all configuration properties from filter items
+			configAgent.AddFilterPropertiesToTreeNode(node,name);
 
-				//printf("Adding node '%s' @ time %ld\n",name.c_str(),time);
-				tree->children->push_back(node);
-			}
-			// Else, use found node
+			//printf("Adding node '%s' @ time %ld\n",name.c_str(),time);
+			tree->children->push_back(node);
+		}
+		// Else, use found node
 
+		if(firstIndex != -1)
+		{
 			// Parse rest of string
 			this->InsertByPathName(node, pathname.substr(firstIndex+1,(pathname.length() - firstIndex - 1)),time,continueDrawingSubLeaves);
 		}
-		//this->InsertByPathName(tree, pathname.substr(firstIndex+1,(pathname.length() - firstIndex - 1)),time,continueDrawingSubLeaves);
 	}
+	//this->InsertByPathName(tree, pathname.substr(firstIndex+1,(pathname.length() - firstIndex - 1)),time,continueDrawingSubLeaves);
 }
 
 void RepositoryAccess::RemoveByPathName(SurrogateTreeNode* tree, string pathname)
@@ -155,10 +139,10 @@ void RepositoryAccess::RemoveByPathName(SurrogateTreeNode* tree, string pathname
 	bool removeNode = false;
 	DiscursiveDebugPrint("STN OP","RemoveByPathName( %s )\n", pathname.c_str());
 	// Split off first part of path
-	int firstIndex = pathname.find("/");
+	size_t firstIndex = pathname.find("/");
 	// If path separator is missing, we only have filename or unadorned dir
 	// If path separator is last character, we are removing a directory
-	if(firstIndex == -1 || firstIndex == pathname.length() - 1 )
+	if((firstIndex == (size_t)-1) || (firstIndex == (size_t)(pathname.length() - 1)) )
 	{
 		removeNode = true;
 	}
