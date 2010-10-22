42ffb21d70af2cc29237ca929cc26b74397ab6b6
hunter@discursivelabs.com <hunter@discursivelabs.com>
2010-10-21 19:21:36 -0700

    added script to run to double lsit of svn repos and repo names

---
+++ ExampleScripts/genImagesFromSvnList.sh
@@ -0,0 +1,22 @@
+#/bin/baash
+
+#// loop through a file, item1= url, item2=name
+cd ~/Projects/source_tree_vis/
+
+ITERATOR=1;
+NAME="noname"
+REPO="norepo"
+for i in $(cat ./ExampleScripts/svnlist.txt); 
+do 
+	let ITERATOR="($ITERATOR + 1)";
+	let evencheck="($ITERATOR%2)"
+	if [ $evencheck -eq 0  ] 
+	then
+   	REPO="$i";
+	else
+   	NAME="$i";
+	./bin/simple_tree -S $REPO -o $NAME
+	fi
+done
+
+#./bin/simple_tree -S http://miranda.googlecode.com/svn/trunk/  -o miranda-im.png && ristretto ./out/*.png
